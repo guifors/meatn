8d336a222b2cc0c8dd6e40f8cf628d038bdf2a1e
Tom Hudson <tom.hudson89@gmail.com>
2020-02-22 15:41:57 +0000

    added the long and lat in

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,56 @@
 class RestaurantsController < ApplicationController
- def index
-    @restaurants = Restaurant.all
+  before_action :find_restaurant, only: [:show, :edit, :update]
+  skip_before_action :authenticate_user!, only: :index
+  def index
+    if params[:city].present?
+      @restaurants = Restaurant.search_by_address(params[:city])
+    else
+      @restaurants = Restaurant.geocoded
+    end
+    set_markers
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit
   end
 
   def show
+    @booking = Booking.new
   end
 
+  def update
+    @restaurant.update(restaurant_params)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:user, :address, :guests, :beds, :bedrooms, :description, :price_per_night, :title, photos: [])
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def set_markers
+    @markers = @restaurants.map do |restaurant|
+      {
+        lat: restaurant.latitude,
+        lng: restaurant.longitude
+      }
+    end
+  end
 end
