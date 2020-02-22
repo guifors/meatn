d076f1be18775120ff1b8ce26628d3839ab2e39e
David Egerton <david.egertonsmith@gmail.com>
2020-02-22 15:07:14 +0000

    new model and schema, updated routes and some bookings show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'group/new'
   devise_for :users
   root to: 'pages#home'
-  resources :bookings
-  resources :restaurants
+  resources :bookings, except: [:new, :create] do
+    resources :groups, only: [:new, :create]
+  end
+
+
+  resources :restaurants do
+    resources :bookings, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
