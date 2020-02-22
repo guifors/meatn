2957f97aca9b2781e82b5f5c2a69af9a10ad23ea
Guillermo Forteza <guifors@gmail.com>
2020-02-22 15:07:08 +0000

    added restaurant views and started to create notifications style

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,26 @@
 class RestaurantsController < ApplicationController
- def index
-    @restaurants = Restaurant.all
+  before_action :find_restaurant, only: [ :show, :edit, :update]
+  skip_before_action :authenticate_user!, only: :index
+  def index
+    if params[:city].present?
+      @restaurants = policy_scope(Restaurant).search_by_city(params[:city])
+      authorize @restaurants
+
+    else
+      @restaurants = policy_scope(Restaurant)
+    end
   end
 
   def show
   end
 
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :postcode, :city, :state, :area, :country, :phone_number, :price, :latitude, :longitude, :image_url)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_city,
+    against: [ :city ],
+    using: {
+      tsearch: { prefix: true }
+  }
   has_many :bookings
 end
