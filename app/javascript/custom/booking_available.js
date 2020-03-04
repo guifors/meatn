80fe38cd55872f0d1d92aeed9e477afd36c71090
Guillermo Forteza <guifors@gmail.com>
2020-03-04 14:25:45 +0000

    Added js to datetime to show booking availability

---
+++ app/javascript/custom/booking_available.js
@@ -0,0 +1,16 @@
+const dateElement = document.getElementById('booking_date_5i');
+
+
+const triggerSuccess = () => {
+  dateElement.addEventListener("change", event => {
+    const bookingSuccess = document.getElementById('booking-success');
+    bookingSuccess.innerHTML = "";
+    bookingSuccess.insertAdjacentHTML("beforeend", `<p class="checking-booking"><i class="fas fa-hourglass-start"> </i>Checking availability...</p>`);
+    setTimeout(function(){
+      bookingSuccess.innerHTML = "";
+      bookingSuccess.insertAdjacentHTML("beforeend", `<p class="booking-success-text"><i class="far fa-check-circle"> </i>Congrats! This restaurant has availability at this time.</p>`);
+    }, 3000);
+  });
+};
+
+export { triggerSuccess };
