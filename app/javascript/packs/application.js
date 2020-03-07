435afe7eaa2582c48e9ac6d7671c548bb6a19f19
Guillermo Forteza <guifors@gmail.com>
2020-03-07 10:05:20 +0000

    improved booking index page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import { initMapbox } from '../plugins/init_mapbox';
 initMapbox();
 
+import { TriggerAttendeeMondal } from 'custom/attendee_modal.js';
+TriggerAttendeeMondal();
+
 import { openModal, closeModal } from '../custom/modal.js';
 import { initStarRating } from 'plugins/init_star_rating';
 import { triggerSuccess } from 'custom/booking_available.js';
